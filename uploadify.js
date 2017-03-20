5e0c908016cd1b464adca7625c234bd59664945b
Fang Jian <fangjianmail@126.com>
2017-03-20 15:56:03 +0800

    use ecstatic to show dir

--- uploadify.js
+++ uploadify.js
@@ -1,7 +1,7 @@
-var express = require('express');
 var multer = require('multer'); // v1.0.5
 var path=require('path');
 var fs = require('fs-extra');
+var ecstatic = require('ecstatic');
 
 function makeFileName(originalname){
 	var extname=path.extname(originalname);
@@ -24,7 +24,8 @@ function uploadify(app,upload_path){
 	})
  
 	var upload = multer({ storage: storage })
-	app.use('/uploads',express.static(upload_path));
+
+	app.use(ecstatic({ root:upload_path,showdir : true,baseDir:'/uploads'}));
 
 	app.post('/upload', upload.array('files'), function (req, res, next) {
 		var file=req.files[0];
